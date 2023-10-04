40e73f0bc06651a617e8d4ee65726bcfa97e8146
Pariksheet <depariksheet2@gmail.com>
2023-10-04 21:20:15 +0530

    --COMMIT

--- Hello.java
+++ Hello.java
@@ -1,8 +1,12 @@
 // Simple Hello World App
+// java is a strongly typed programming language, because we need to define the type of varibale to store the data
 class Hello
 {
-    public static void main(String a[])
+    public static void main(String arg[])
     {
-        System.out.print("Hello World from Pariksheet");
+        int a = 7;
+        int b = 3;
+        System.out.println("Hello World from Pariksheet");
+        System.out.print(a + b);
     }
 }
