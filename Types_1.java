33593b0e69bc0c524c50062444beb2a9f6fa3ffb
Pariksheet <deparikshee2@gmail.com>
2023-10-05 20:37:01 +0530

    COMMIT

---
+++ Types_1.java
@@ -0,0 +1,10 @@
+public class Types_1 {
+    public static void main(String args[])
+    {
+        // working with literals
+        int a = 0x8E;
+        int b = 100_00_000;
+        System.out.println(a);
+        System.out.println(b);
+    }
+}
