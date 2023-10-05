20c1e579df6340d793a907107f0852adb8b7d21c
Pariksheet <deparikshee2@gmail.com>
2023-10-05 19:45:46 +0530

    --COMMIT

---
+++ Types.java
@@ -0,0 +1,19 @@
+public class Types {
+    public static void main(String args[])
+    {
+        int a = 10;
+        double b = 12.548787;
+        float c = 12.45456f;
+        long d = 24564l;
+        char word = 'A';
+        boolean name = true;
+
+        System.out.println(a);
+        System.out.println(b);
+        System.out.println(c);
+        System.out.println(d);
+        System.out.println(word);
+        System.out.println(name);
+    }
+   
+}
